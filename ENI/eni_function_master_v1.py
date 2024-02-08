a10d03872a858c505b226bb54310846205a8a276
Kevin Thiel <kevin.thiel@ou.edu>
2024-02-08 14:57:33 -0600

    Test1 for new ENI dataset

--- ENI/eni_function_master_v1.py
+++ ENI/eni_function_master_v1.py
@@ -148,7 +148,55 @@ def eni_loader(start_time, end_time, input_loc):
         df = pd.concat((df,new_df),axis=0)
         
     return df
+
+def eni_loader_v2(start_time, end_time, input_loc):
+    '''
+    A function for collecting ENI data from a specified time period and range (CONUS), and putting it into a single dataframe
+    PARAMS:
+        start_time: Beginning of the time period (datetime object)
+        end_time: End of the time period (datetime object)
+        input_loc: Location of the ENI data (str)
+    RETURNS:
+        df: The compiled flash data from the csv files (DataFrame)
+    '''
+    #Creating a list of times to rotate through
+    time_list = pd.date_range(start=start_time, end=end_time,freq='1D').to_list()
+    
+    #Empty DataFrame that we'll fill
+    df = pd.DataFrame(columns=('type','timestamp','latitude','longitude','peakcurrent','icheight','numbersensors','icmultiplicity','cgmultiplicity','starttime','endtime','duration','ullatitude','ullongitude','lrlatitude','lrlongitude'))
+  
+    #Looping through the list of available times
+    for cur_time in time_list:
+        y,m,d,doy,hr,mi = datetime_converter(cur_time) #Turning the current time into a string
         
+        #Specifying the folder by date
+        file_loc = input_loc + y + m + d + '/'
+        #file_loc = input_loc #DEVMODE
+        
+        #Creating the file string we'll use in the glob function
+        file_str = 'eni_flash_flash'+y + m + d + '.csv'
+        
+        #Collecting the files on the given day
+        collected_file = sorted(glob(file_loc+file_str))
+        
+        cfile_str = collected_file[0]
+
+        #Reading in the collected file
+        new_df = pd.read_csv(cfile_str)
+
+        #Removing the data outside of the domain of study
+        bound_index = latlon_bounds(new_df['latitude'].values, new_df['longitude'].values)
+        new_df = new_df.iloc[bound_index,:]
+
+        #Adding the current file string to the new dataframe so we can find it more easily later
+        file_str_list = np.full(new_df.shape[0], file_str)
+        new_df['File_String'] = file_str_list
+
+        #Appending the new DataFrame to the combined one
+        df = pd.concat((df,new_df),axis=0)
+        
+    return df
+
 
 def time_str_converter(time_str):
     '''
@@ -243,6 +291,62 @@ def eni_ff_hunter(df, search_start_time, search_end_time, search_r, search_m):
                            (df_cut['Latitude'] >= (df_search.loc[i]['Latitude']-dx)) &
                            (df_cut['Longitude'] <= (df_search.loc[i]['Longitude']+dx)) &
                            (df_cut['Longitude'] >= (df_search.loc[i]['Longitude']-dx))]
+
+        #Setting up and running a ball tree
+        btree = BallTree(df_cut[['lat_rad','lon_rad']].values, leaf_size=2, metric='haversine')
+        indicies = btree.query_radius([c_pt], r = search_r/R)
+
+        #If only the point itself is returned within the search distance, then
+        if len(indicies[0])==1:
+            ff_df = pd.concat((ff_df,df.loc[df.index==i]),axis=0)
+            
+    return ff_df
+
+def eni_ff_hunter_v2(df, search_start_time, search_end_time, search_r, search_m):
+    '''
+    Funciton used to identify first flash events. Using the Ball Tree from scikitlearn
+    https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.BallTree.html
+    Adapted from first-flash/GLM/first_flash_funtion_master.py:ff_hunter
+    PARAMS:
+        df: Dataframe containing the start_time, lat, lon, lat_rad, lon_rad
+        search_start_time: start time used to depict which flashes are being investigated
+        search_end_time: end time used to depict which flashes are being investigated
+        search_r: Search radius of the ball tree (km)
+        search_m: Time window to search in (minutes)
+    RETURNS:
+        ff_df: A dataframe containing only the first flash events
+    '''
+    R = 6371.0087714 #Earths radius in km
+    
+    #Need to institute this below
+    t_delta = timedelta(minutes=search_m)
+
+    ff_df = pd.DataFrame(columns=('type','timestamp','latitude','longitude','peakcurrent','icheight','numbersensors','icmultiplicity','cgmultiplicity','starttime','endtime','duration','ullatitude','ullongitude','lrlatitude','lrlongitude'))
+    
+    #Pruning our indicies to only the ones that are after the current search timeframe
+    start_time = df['starttime'].values #Adding the flash start times (as datetime objects) to the dataframe
+    df['start_time'] = time_str_converter(start_time)
+    df_search = df.loc[(df['start_time'] >= search_start_time) & (df['start_time'] <= search_end_time)]
+    
+    #This loop goes through based upon the index of the provided dataframe and finds the first flashes
+    #The output is a dataframe of first flash events 
+    for i in df_search.index.values:
+
+        #Getting the current lat, lon, and index
+        c_pt = df.loc[i][['lat_rad','lon_rad']].values
+        c_stime = df.loc[i][['start_time']].values[0]
+
+        #Removing the flashes that happened 30+ min before and anything after the current flash from consideration
+        time_prev = df.loc[i]['start_time'] - t_delta #Finding the time from the previous 30 minutes
+        df_cut = df.loc[(df.loc[i][['start_time']].values[0] >= df['start_time']) & 
+                         (df['start_time'] >= time_prev)]
+          
+        #Making a smaller tree to reduce the required memory (and increase speed) of the ball tree
+        dx = 0.5 #Change in latitude max. Using a blanket benchmark to reduce the number of distance calculations made
+        df_cut = df_cut.loc[(df_cut['Latitude'] <= (df_search.loc[i])['latitude']+dx) &
+                           (df_cut['latitude'] >= (df_search.loc[i]['latitude']-dx)) &
+                           (df_cut['longitude'] <= (df_search.loc[i]['longitude']+dx)) &
+                           (df_cut['longitude'] >= (df_search.loc[i]['longitude']-dx))]
 
         #Setting up and running a ball tree
         btree = BallTree(df_cut[['lat_rad','lon_rad']].values, leaf_size=2, metric='haversine')
