5e1fbb66574f953f55bbbff9d93a5f4f932350e3
Kevin Thiel <kevin.thiel@ou.edu>
2024-03-11 16:54:25 -0500

    More debugging

--- GLM/first_flash_function_master.py
+++ GLM/first_flash_function_master.py
@@ -499,7 +499,7 @@ def ff_hunter_gridsearch(df, search_start_time, search_end_time, search_r, searc
         if search_flash_r == 0: #If there's no value for the flash radius value, then use the traditional radius values
             indicies = btree.query_radius([c_pt], r = search_r/R)
         elif search_r == 0: #If the traditional radius value is zero, then use the circular radius of the flash area plus a buffer
-            c_area = df.loc[i][['flash_area']].values
+            c_area = df.loc[i][['flash_area']].values[0]
             print (c_area)
             flash_r_from_area = np.sqrt((c_area/1000000)/np.pi) #Getting the radius (km) from the flash area in sq meters
             indicies = btree.query_radius([c_pt], r = (search_flash_r + flash_r_from_area)/R)
