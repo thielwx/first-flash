8ea6631d58ae44f6fa3ad51b75eec183730d98e8
Kevin Thiel <kevin.thiel@ou.edu>
2024-06-13 15:44:46 -0500

    First test of creating the raw files for each case

--- manual-analysis-v1/GLM-ff-raw-creator-gridsearch.py
+++ manual-analysis-v1/GLM-ff-raw-creator-gridsearch.py
@@ -11,26 +11,23 @@
 # Note: Script was adapted from GLM-ff-raw-creator-v2.py
 #
 # Author: Kevin Thiel
-# Created: March 2024
+# Created: June 2024
 # Email: kevin.thiel@ou.edu
 # 
 #
-# Inputs:
-#      start time YYYYmmddHHMM
-#      end time YYYYmmddHHMM
-#      GLM number
+#
 #================================================================================================
 
 # In[9]:
 
-
+import yaml
 import netCDF4 as nc
 import pandas as pd
 import numpy as np
 from datetime import datetime
 from datetime import timedelta
 import sys
-import first_flash_function_master as ff
+import manual_analysis_function_master as ff
 import multiprocessing as mp
 import os
 from glob import glob
@@ -40,24 +37,21 @@ from glob import glob
 # In[3]:
 
 
-args = sys.argv
-#args = ['BLANK','202205040000','202205050000','16'] #casename,start date, end date, glm name #DEV MODE
+#Importing the appropriate yaml file
+with open('case-settings-manual-analysis.yaml', 'r') as f:
+    sfile = yaml.safe_load(f)
 
-start_time_str = args[1]
-end_time_str = args[2]
-glm_sat = args[3]
+cases = sfile['cases']
 
-#Converting the time strings to datetimes
-start_time = datetime.strptime(start_time_str, '%Y%m%d%H%M')
-end_time = datetime.strptime(end_time_str, '%Y%m%d%H%M')
 
 # Getting all of the combinations that we'll be searching
 search_combos = pd.read_csv('ff_gridsearch_combinations.csv', index_col=0)
 
 #Constants
-dt = timedelta(minutes=50) #Set to the max possible serach time period
+dt = timedelta(minutes=50) #Set to the max possible search time period
+dt30 = timedelta(minutes=31) #Set for the end times
 ver = 1
-
+glm_sat = 16
 
 # # Function Land
 
@@ -79,7 +73,12 @@ def first_flash_multiprocessor(start_time, end_time):
     '''
     #Making a list of times in two hour chunks
     time_list = pd.date_range(start=start_time, end=end_time, freq='2H')
+    pool_size = len(time_list)
     
+    #Ensuring the pool size doesn't exceed the number of available CPUs
+    if pool_size > 12:
+        pool_size = 12
+
     if __name__ == "__main__":
         with mp.Pool(12) as p:
             p.starmap(ff_driver,zip(time_list[:-1],time_list[1:]))
@@ -90,7 +89,7 @@ def first_flash_multiprocessor(start_time, end_time):
 # In[5]:
 
 
-def ff_raw_saver(ff_df, s_time, e_time, version, glm_sat, search_r, search_m, search_flash_r, i):
+def ff_raw_saver(ff_df, s_time, e_time, version, glm_sat, search_r, search_m, search_flash_r, i, case):
     '''
     A function for saving out the raw files
     PARAMS:
@@ -116,12 +115,12 @@ def ff_raw_saver(ff_df, s_time, e_time, version, glm_sat, search_r, search_m, se
     ctime_str = 'c'+y+m+d+hr+mi
     
     #The beginning string for the file name
-    front_string = 'GLM'+str(glm_sat)+'_ffRAW_r'+str(int(search_r))+'_t'+str(int(search_m))+'_fr'+str(int(search_flash_r))+'_v'+str(i).zfill(2)
+    front_string = case+'_GLM'+str(glm_sat)+'_ffRAW_r'+str(int(search_r))+'_t'+str(int(search_m))+'_fr'+str(int(search_flash_r))+'_v'+str(i).zfill(2)
     
     #Creating the entire save string
     save_str = front_string+'_'+stime_str+'_'+ctime_str+'.csv'
     
-    save_loc = '/localdata/first-flash/data/GLM-gridsearch-'+str(version)+'/GLM'+str(glm_sat)+'_ffRAW_v'+str(i).zfill(2)+'/'+output_date_str+'/'
+    save_loc = '/localdata/first-flash/data/manual-analysis-v'+str(version)+'/'+case+'/GLM'+str(glm_sat)+'_ffRAW_v'+str(i).zfill(2)+'/'
     
     print (save_loc+save_str)
     if not os.path.exists(save_loc):
@@ -136,20 +135,21 @@ def ff_driver(s_time, e_time):
     #Grabbing all the extra variables
     global ver
     global df
-    global glm_sat
     global search_m
     global search_r
     global search_flash_r
     global i
+    global case
+    global bounds
 
     #Getting the first flashes for the time period of interest, output as a dataframe
-    ff_df = ff.ff_hunter_gridsearch(df, s_time, e_time, search_r, search_m, search_flash_r)
+    ff_df = ff.ff_hunter_gridsearch(df, s_time, e_time, search_r, search_m, search_flash_r, bounds)
     
     ff_df = ff.ff_next_flashes(df, ff_df, s_time, e_time, 30, 30) #Keeping those at 30 min/30 km for consistency
     
     #ff_df.index.names['fistart_flid'] #Trying to force the index to take on the correct names before saving it
 
-    ff_raw_saver(ff_df, s_time, e_time, ver, glm_sat, search_r, search_m, search_flash_r, i)
+    ff_raw_saver(ff_df, s_time, e_time, ver, search_r, search_m, search_flash_r, i, case)
 
 
 # # Driver section
@@ -157,18 +157,44 @@ def ff_driver(s_time, e_time):
 # In[8]:
 
 
-file_list = ff.data_loader_list(start_time=start_time-dt, end_time=end_time+dt, glm_sat=glm_sat)
-print ('File List Created')
 
-df = ff.data_loader_gridsearch(file_list)
-print ('DataFrame Created')
 
-#Looping through each combination of search criterion
-for i in search_combos.index:
-    search_m = search_combos['minutes'][i]
-    search_r = search_combos['simple_radius'][i]
-    search_flash_r = search_combos['flash_area_radius'][i]
-    print ('Combination: '+str(i))
-    first_flash_multiprocessor(start_time, end_time)
+#Outerloop for cases
+for case in cases[:1]:
+    print ('=========================')
+    print ('START OF CASE: '+case)
+    print ('=========================')
 
-print ('First Flashes Found')
+    #Getting the case times
+    start_time_str = sfile[case]['start_time']
+    end_time_str = sfile[case]['end_time']
+    #Converting the time strings to datetimes
+    start_time = datetime.strptime(start_time_str, '%Y%m%d-%H%M')
+    end_time = datetime.strptime(end_time_str, '%Y%m%d-%H%M')
+
+    #Getting the lats and lons
+    ul_lat = sfile[case]['ul_lat']
+    ul_lon = sfile[case]['ul_lon']
+    lr_lat = sfile[case]['lr_lat']
+    lr_lon = sfile[case]['lr_lon']
+    bounds = [ul_lat,ul_lon,lr_lat,lr_lon]
+
+    #Getting the list of files
+    file_list = ff.data_loader_list(start_time=start_time-dt, end_time=end_time+dt30,glm_sat=glm_sat)
+    print ('File List Created')
+    #Getting the flashes and putting them into a dataframe for the case ()
+    df = ff.data_loader_gridsearch(file_list,bounds)
+    print ('DataFrame Created')
+
+    #Looping through each combination of search criterion
+    for i in search_combos.index[:1]:
+        search_m = search_combos['minutes'][i]
+        search_r = search_combos['simple_radius'][i]
+        search_flash_r = search_combos['flash_area_radius'][i]
+        print ('Combination: '+str(i))
+        print ('serach_m: '+str(search_m))
+        print ('search_r: '+str(search_r))
+        print ('search_flash_r: '+str(search_flash_r))
+        first_flash_multiprocessor(start_time, end_time)
+
+print ('DING! All First-Flashes Found')
