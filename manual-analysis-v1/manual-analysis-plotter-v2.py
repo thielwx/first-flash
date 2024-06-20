db68a933e5d5297ff04a98560106e7b467e01c16
Kevin Thiel <kevin.thiel@ou.edu>
2024-06-20 16:29:33 -0500

    First attempt at a plotting script

--- manual-analysis-v1/manual-analysis-plotter-v2.py
+++ manual-analysis-v1/manual-analysis-plotter-v2.py
@@ -112,10 +112,34 @@ def LMA_times_postprocess(file_times, times):
     return (flash_datetime)
 
 
+def eni_puller(start_time_str, end_time_str):
+    '''
+    Pulls data from the current and next day since you have to load in multipe files (days) for a case
+    PARAMS: 
+        start_time_str: String of the start time from the settings file
+        end_time_str: String of the end time from the settings file
+
+    '''
+    #Getting the basic data to use later
+    data_loc = '/localdata/first-flash/data/ENI-base-stock/eni_flash_flash'
+
+    #Loading in the data to a single dataframe
+    if start_time_str[:8] != end_time_str[:8]:
+        eni1 = pd.read_csv(data_loc+start_time_str+'.csv', index_col=0)
+        eni2 = pd.read_csv(data_loc+end_time_str+'.csv', index_col=0)
+        eni = pd.concat((eni1,eni2), axis=0)
+    else:
+        eni = pd.read_csv(data_loc+start_time_str[:8]+'.csv', index_col=0)
+
+    return eni
+
+    
+
+
 # In[5]:
 
 
-def abi_puller(t, abi_meso_num):
+def abi_puller(t):
     #Getting the necessary ABI data
     CMI = [[-999]]
     x = [np.nan]
@@ -131,7 +155,7 @@ def abi_puller(t, abi_meso_num):
     cur_date = t.strftime('%Y%m%d')
     file_time_string = t.strftime('%Y%j%H%M')
     
-    cur_abi = 'ABI16-CMIPM13-'+abi_meso_num
+    cur_abi = 'ABI16-CMIPC13'
     file_loc = '/localdata/first-flash/data/'+cur_abi+'/'+cur_date+'/*s'+file_time_string+'*.nc'
     #print (file_loc)
     collected_files = glob(file_loc)
@@ -141,7 +165,6 @@ def abi_puller(t, abi_meso_num):
         CMI = dset.variables['CMI'][:]
         CMI[CMI>280] = np.nan
         sat_lon = dset.variables['goes_imager_projection'].longitude_of_projection_origin
-        sat_sweep_axis = dset.variables['goes_imager_projection'].sweep_angle_axis
         sat_h = dset.variables['goes_imager_projection'].perspective_point_height
         geo_crs = ccrs.Geostationary(central_longitude=sat_lon,satellite_height=sat_h)
         
@@ -227,7 +250,7 @@ def mrms_puller(t,cur_lat,cur_lon):
 def save_string(cur, i, t, case):
     fistart_flid = cur['fistart_flid']
     time_str = t.strftime('%Y%m%d-%H%M%S')
-    start_str = '/localdata/first-flash/figures/manual-analysis/'+case+'/'+str(i)+'-'+fistart_flid +'/'
+    start_str = '/localdata/first-flash/figures/manual-analysis-v1/'+case+'/'+str(i)+'-'+fistart_flid +'/'
     save_str = start_str + time_str + '.png'
 
     # Uncomment when running on devlab4
@@ -240,17 +263,19 @@ def save_string(cur, i, t, case):
 # In[15]:
 
 
-def plotter(cur, dx, i, case):
+def plotter(cur, dx, i, case, g16, eni):
     cur_time = cur['time64']
     cur_lat = cur['lat']
     cur_lon = cur['lon']
     cur_area = cur['flash_area'] / 1e6 #Putting into km sq
     
-    #Creating the time list. Every minute, along with every ten seconds in the final minute
+    # Creating the time list. Every minute, along with every ten seconds in the final minute 
+    # and the five seconds before and after the first flash event
     time_list_start = pd.date_range(start=cur_time-dt10, end=cur_time-np.timedelta64(1,'m'), freq='60s').tolist()
     time_list_mid = pd.date_range(start=cur_time-dt50s, end=cur_time-np.timedelta64(10,'s'), freq='10s').tolist()
-    time_list_end = pd.date_range(start=cur_time, end=cur_time+np.timedelta64(5,'m'), freq='60s').to_list()
-    time_list = time_list_start+time_list_mid+time_list_end
+    time_list_mid2 = pd.date_range(start=cur_time-np.timedelta64(5,'s'), end=cur_time+np.timedelta64(5,'s'), freq='5s').tolist()
+    time_list_end = pd.date_range(start=cur_time, end=cur_time+np.timedelta64(5,'m'), freq='60s').to_list()[1:]
+    time_list = time_list_start+time_list_mid+time_list_mid2+time_list_end
     
     #Caucluations for the approx area of the first flash event for continuity
     R = 6371.0087714 #Earths radius in km
@@ -265,16 +290,13 @@ def plotter(cur, dx, i, case):
         g16_cut = g16.loc[(g16['lat']>=cur_lat-dx)&(g16['lat']<=cur_lat+dx)&
                          (g16['lon']>=cur_lon-dx)&(g16['lon']<=cur_lon+dx)&
                          (g16['time64']>=t-dt10)&(g16['time64']<=t)]
-        lma_cut = lma.loc[(lma['ctr_lat']>=cur_lat-dx)&(lma['ctr_lat']<=cur_lat+dx)&
-                         (lma['ctr_lon']>=cur_lon-dx)&(lma['ctr_lon']<=cur_lon+dx)&
-                         (lma['time64']>=t-dt10)&(lma['time64']<=t)]
         eni_cut = eni.loc[(eni['latitude']>=cur_lat-dx)&(eni['latitude']<=cur_lat+dx)&
                          (eni['longitude']>=cur_lon-dx)&(eni['longitude']<=cur_lon+dx)&
                          (eni['time64']>=t-dt10)&(eni['time64']<=t)]
         
         #=====================================================
         #Step 2: Getting the data from the MRMS and ABI
-        CMI, x, y, abi_extent, geo_crs = abi_puller(t, abi_meso_num)
+        CMI, x, y, abi_extent, geo_crs = abi_puller(t)
         refl_10, mrms_extent = mrms_puller(t, cur_lat, cur_lon)
         save_str = save_string(cur, i, t, case)
         
@@ -317,10 +339,11 @@ def plotter(cur, dx, i, case):
         ax2.add_feature(USCOUNTIES, edgecolor='g', zorder=0)
         ax2.add_feature(cfeature.STATES, edgecolor ='r',linewidth=1.5, zorder=0)
         
-        ax2.scatter(x=lma_cut['ctr_lon'], y=lma_cut['ctr_lat'], c='b', s=tf_size, marker='.', zorder=3, label='LMA Flashes (10 min.)')
+        ax2.scatter(x=g16_cut['lon'], y=g16_cut['lat'], transform=plt_car_crs, alpha=1, label='GLM16 Flashes (10 min.)', marker='.', s=tf_size, c='r')
+        ax2.scatter(x=eni_cut['longitude'], y=eni_cut['latitude'], transform=plt_car_crs, alpha=1, label='ENI Flashes (10 min.)', marker='o', s=tf_size, c='k')
         ax2.scatter(x=cur_lon, y=cur_lat, transform=plt_car_crs, c='k', s=150, marker='x', zorder=2, alpha=0.25)
         ax2.legend(loc='upper right')
-        ax2.set_title('LMA / -10C dBZ')
+        ax2.set_title('GLM/ ENI / -10C dBZ')
         
         if refl_10[0,0]!=-999:
             b = ax2.imshow(refl_10, extent=mrms_extent, transform=plt_car_crs, cmap=plt.get_cmap('turbo', 30), vmin=10, vmax=60, zorder=0, alpha=0.6)
@@ -337,11 +360,10 @@ def plotter(cur, dx, i, case):
 # In[9]:
 
 #Outerloop on a per-case basis
-for case in cases:
+for case in cases[:1]:
 
     ff_loc = '/localdata/first-flash/data/manual-analysis-v1/'+case+'/'
-    glm16_all_file = sfile[case][glm16_all]
-
+    glm16_all_file = sfile[case]['glm16_all']
 
     #Reading in the datasets and getting the time data necessary
     ff = pd.read_csv(ff_loc+case+'-ffRAW-v00combos-v1.csv', index_col=0)
@@ -350,16 +372,12 @@ for case in cases:
     g16 = pd.read_csv('/localdata/first-flash/data/GLM16-cases-allflash/'+glm16_all_file, index_col=0)
     g16['time64'] = [np.datetime64(i) for i in g16['start_time'].values]
 
-    eni = pd.read_csv(eni_loc, index_col=0)
+    eni = eni_puller(sfile[case]['start_time'],sfile[case]['end_time'])
     eni['time64'] = [np.datetime64(i) for i in eni['timestamp'].values]
 
-
-# In[16]:
-
-
-#Looping through each first flash event
-for i in range(ff.shape[0]):
-    print (str(i+1)+'/'+str(ff.shape[0]))
-    cur = ff.iloc[i]
-    
-    plotter(cur, dx, i, case)
+    #Looping through each first flash event
+    for i in range(ff.shape[0])[:1]:
+        print (str(i+1)+'/'+str(ff.shape[0]))
+        cur = ff.iloc[i]
+        
+        plotter(cur, dx, i, case, g16, eni)
