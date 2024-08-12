79d56985363af431b5f48242fa939c2b8117a2fd
Kevin Thiel <kevin.thiel@ou.edu>
2024-08-12 16:04:58 -0500

    Preparing to run only the 20220921-ut case

--- manual-analysis-v1/GLM-ff-raw-creator-gridsearch.py
+++ manual-analysis-v1/GLM-ff-raw-creator-gridsearch.py
@@ -157,7 +157,7 @@ def ff_driver(s_time, e_time):
 
 
 #Outerloop for cases
-for case in cases:
+for case in cases[-2:-1]:
     print ('=========================')
     print ('START OF CASE: '+case)
     print ('=========================')
