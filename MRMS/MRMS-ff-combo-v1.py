5e470a4a309e5fb806942618e6002932b381fd53
Kevin Thiel <kevin.thiel@ou.edu>
2024-09-10 16:18:12 -0500

    Initial commit of MRMS directory

--- MRMS/MRMS-ff-combo-v1.py
+++ MRMS/MRMS-ff-combo-v1.py
@@ -114,7 +114,7 @@ def mrms_driver(t_start, t_end):
     #A 2-minute time list to go through each  mrms file
     tlist_2min = pd.date_range(start=t_start, end=t_end, freq='120s')
     
-    df_locs = np.where((f_time>=t_start) & (f_time<t_end))[0]
+    df_locs = np.where((f_time>=np.datetime64(t_start)) & (f_time<np.datetime64(t_end)))[0]
 
     #Creating an empty dataframe to fill
     df = pd.DataFrame(index=fistart_flid[df_locs], columns=mrms_variables_output)
