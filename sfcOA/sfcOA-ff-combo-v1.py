ed12939c5313ba36c9d0b667d3ccf6db71aafd20
Kevin Thiel <kthiel@badgers.spc.noaa.gov>
2025-10-14 15:49:48 +0000

    Testing I can upload from new computer

--- sfcOA/sfcOA-ff-combo-v1.py
+++ sfcOA/sfcOA-ff-combo-v1.py
@@ -1,1 +1,11 @@
-#Here's some text
+#================================================
+# This script uses first flash locations and find sfcOA data
+# Create: October 2025
+# Author: Kevin Thiel (kevin.thiel@ou.edu)
+#================================================
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
