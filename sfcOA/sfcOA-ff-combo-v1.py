74c2c84734d7974c829006417743ce896d2c7433
Kevin Thiel <kthiel@badgers.spc.noaa.gov>
2025-10-29 16:43:51 +0000

    Test of setup

--- sfcOA/sfcOA-ff-combo-v1.py
+++ sfcOA/sfcOA-ff-combo-v1.py
@@ -91,6 +91,9 @@ def sfc_oa_file_times(f_time):
 
     return oa_times_t0, oa_times_t1, oa_times_t2, oa_times_t3
     
+#Allows us to open one file at a time to fill the dataframe
+def oa_df_filler(df, oa_vars_input, oa_vars_output, t0_locs, t1_locs,  t2_locs, t3_locs, oa_lats, oa_lons, oa_data, fistart_flid, f_lat, f_lon):
+	
 
 
 #====================================================
@@ -100,6 +103,7 @@ def sfcoa_driver(t_start, t_end):
     #Loading in the other data to the function
     global oa_vars_input
     global oa_vars_output
+	global oa_files_loc
     global version
     global fstring_start
     global f_time
@@ -122,7 +126,42 @@ def sfcoa_driver(t_start, t_end):
         #Getting the list of file names needed based on the first flash times for T0 T1 T2 T3
         oa_times_t0, oa_times_t1, oa_times_t2, oa_times_t3 = sfc_oa_file_times(f_time[df_locs])
 
-        #STOP POINT
+        #Making list of files we need to loop through
+		oa_file_loop_list = [t.strftime('sfcoaruc_%y%m%d%H') for t in pd.date_range(start=t_start-timedelta(minutes=180, end=t_end, freq='1h')]
+
+		#Looping through each potneital file that we need to pull from
+		for cur_oa_file in oa_file_loop_list:
+
+			#checking that the path exists
+			file_truther = os.path.exists(oa_files_loc+cur_oa_file)
+
+			#Finding where we need to sample for each time lag
+			t0_locs = np.where(oa_times_t0 == cur_oa_file)[0]
+			t1_locs = np.where(oa_times_t1 == cur_oa_file)[0]
+			t2_locs = np.where(oa_times_t2 == cur_oa_file)[0]
+			t3_locs = np.where(oa_times_t3 == cur_oa_file)[0]
+			t_lens = [len(t0_locs), len(t1_locs), len(t2_locs), len(t3_locs)]
+
+			#If no file exists, put the data in as dummy variables			
+			if (file_truther == False):
+				print ('ERROR: NO FILE EXISTS - '+cur_oa_file)
+				oa_lats = [-999]
+				oa_lons = [-999]
+				oa_data = [-999]
+			#If the oa data file isn't needed then skip in the loop
+			elif (np.sum(t_lens)==0):
+				continue
+			#If data exists, open the file!
+			else:
+				oa_data = gpk.GempakGrid(oa_files_loc+cur_oa_file)
+				oa_lats = oa_data.lat
+				oa_lons = oa_data.lon
+				print ('DING!')
+				print (cur_oa_file+' read')
+			
+			#Shipping all this stuff off to sample and fill the dataframe
+			#df = oa_df_filler(df, oa_vars_input, oa_vars_output, t0_locs, t1_locs,  t2_locs, t3_locs, oa_lats, oa_lons, oa_data, fistart_flid[df_locs], f_lat[df_locs], f_lon[df_locs])
+				
 
 # WORK ZONE
 
@@ -157,7 +196,7 @@ for i in range(len(time_list_days)-1):
     t_range_end = time_list_days[i+1]
     
     #Breaking the day into 12, 2-hour chunks
-    tlist_starmap = pd.date_range(start=t_range_start, end=t_range_end, freq='2H')
+    tlist_starmap = pd.date_range(start=t_range_start, end=t_range_end, freq='24H')
 
 	#Sending the file string to the sfcoa_driver function that takes over from here...
     if __name__ == "__main__":
