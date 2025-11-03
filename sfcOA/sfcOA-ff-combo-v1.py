d0183986f59846684b9c00bc641a13592a0de95f
Kevin Thiel <kthiel@badgers.spc.noaa.gov>
2025-11-03 21:04:38 +0000

    Debugging

--- sfcOA/sfcOA-ff-combo-v1.py
+++ sfcOA/sfcOA-ff-combo-v1.py
@@ -171,6 +171,7 @@ def oa_df_filler(df, oa_vars_input, oa_vars_output, t0_locs, t1_locs, t2_locs, t
 			for loc in t3_locs:
 				#Getting the index to sample on the oa grid
 				oa_loc = oa_ff_locs[loc]
+				print (oa_loc)
 				#Sampling the sfc oa data and placing the value in the dataframe
 				df.loc[fistart_flid[loc],var+'_T3'] = np.array(oa_data).flatten('C')[oa_loc]
 
