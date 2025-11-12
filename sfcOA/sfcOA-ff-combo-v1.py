868e86d3636b78e5f5e761cf532cf4b1972c787c
Kevin Thiel <kthiel@badgers.spc.noaa.gov>
2025-11-12 21:17:56 +0000

    New variables

--- sfcOA/sfcOA-ff-combo-v1.py
+++ sfcOA/sfcOA-ff-combo-v1.py
@@ -22,7 +22,7 @@ import warnings
 warnings.filterwarnings('ignore') 
 
 #Constants
-version = 2
+version = 1
 oa_files_loc = '/localdata/first-flash/data/sfcOA-local/'
 ff_combo_loc = '/localdata/first-flash/data/GLM-ff-east-west-combined/GLM-East-West_first-flash-data_v32_s202201010000_e202301010000_c202410071459.nc'
 dt_start_job = datetime.now()
